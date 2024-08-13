1ed39f253fc5c6ab58fd8db7108ce6f28b5fd560
Emma24968 <118956147+Emma24968@users.noreply.github.com>
2024-08-13 23:54:11 +0100

    Delete javascript30 directory

--- javascript30/day12/train.js
+++
@@ -1,9 +0,0 @@
-function welcomeUser(params) {
-    var traintype = document.getElementById('traintype').value;
-    var Origin = document.getElementById('origin').value;
-    var date = document.getElementById('date').value;
-    var destination= document.getElementById('destination').value;
-
-    var welcomeMessge = 'Our dear customer, note that you selected' +  traintype + 'train departing from' + origin + 'travelling to' + destination + 'and your date of travel is' + date + '. Thank you for choosin us. Safe journey!.';
-    document.getElementById('welcomeMessage').textContent=welcomeMessge;
-}
